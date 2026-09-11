8f6a4a8c26af8bb9727ecd57673561d209c5bd15
GalibDev <mirza.galib.palash@gmail.com>
2026-09-11 23:32:53 +0600

    Add Google sign-in and professional login design

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc, getDocs, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 import { firebaseConfig } from "./firebase-config.js";
 const ready = Boolean(firebaseConfig.apiKey && firebaseConfig.projectId && firebaseConfig.appId); let auth, db;
@@ -8,6 +8,7 @@ export const firebaseReady = ready;
 export const observeAuth = callback => ready ? onAuthStateChanged(auth, callback) : callback(null);
 export const register = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const loginWithGoogle = () => signInWithPopup(auth, new GoogleAuthProvider());
 export const logout = () => signOut(auth);
 export const saveProfile = user => setDoc(doc(db, "users", user.uid), { email: user.email, updatedAt: serverTimestamp() }, { merge: true });
 export async function loadProgress(uid) { const result = await getDoc(doc(db, "progress", uid)); return result.exists() ? result.data().state : null; }
